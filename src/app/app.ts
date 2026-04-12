1d5f467bf3529cc3f7979e69c6e2c23fe12cd1d4
JasonLuis <jason20luis@gmail.com>
2026-04-12 15:40:10 -0300

    feat: adiciona ItemCertificado ao módulo do app

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,10 +4,11 @@ import { Navbar } from "./_components/navbar/navbar";
 import { CommonModule } from '@angular/common';
 import { PrimaryButton } from "./_components/primary-button/primary-button";
 import { SecondaryButton } from "./_components/secondary-button/secondary-button";
+import { ItemCertificado } from "./_components/item-certificado/item-certificado";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, CommonModule, PrimaryButton, SecondaryButton], // CommonModule é necessário para usar *ngIf
+  imports: [RouterOutlet, Navbar, CommonModule, PrimaryButton, SecondaryButton, ItemCertificado], // CommonModule é necessário para usar *ngIf
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
