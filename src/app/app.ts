726e912592cf9225f7f8d54524f9e07a693e83f0
JasonLuis <jason20luis@gmail.com>
2026-04-12 15:05:35 -0300

    feat: adiciona componentes PrimaryButton e SecondaryButton ao app

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,12 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from "./_components/navbar/navbar";
 import { CommonModule } from '@angular/common';
+import { PrimaryButton } from "./_components/primary-button/primary-button";
+import { SecondaryButton } from "./_components/secondary-button/secondary-button";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, CommonModule], // CommonModule é necessário para usar *ngIf
+  imports: [RouterOutlet, Navbar, CommonModule, PrimaryButton, SecondaryButton], // CommonModule é necessário para usar *ngIf
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
