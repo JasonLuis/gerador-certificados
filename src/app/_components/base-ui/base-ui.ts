84ddc1cd13da0864d3799426cacad91c6737c499
JasonLuis <jason20luis@gmail.com>
2026-04-15 00:01:54 -0300

    feat: adiciona componente BaseUi com estrutura inicial

---
+++ src/app/_components/base-ui/base-ui.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { Navbar } from "../navbar/navbar";
+import { PrimaryButton } from "../primary-button/primary-button";
+import { SecondaryButton } from "../secondary-button/secondary-button";
+import { ItemCertificado } from "../item-certificado/item-certificado";
+
+@Component({
+  selector: 'app-base-ui',
+  imports: [],
+  templateUrl: './base-ui.html',
+  styleUrl: './base-ui.css',
+})
+export class BaseUi {}
