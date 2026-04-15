b561bd8264212a8033abefb6dd594acce1c0c1b6
JasonLuis <jason20luis@gmail.com>
2026-04-15 00:01:27 -0300

    feat: adiciona BaseUi e Certificados ao módulo principal

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,10 +5,12 @@ import { CommonModule } from '@angular/common';
 import { PrimaryButton } from "./_components/primary-button/primary-button";
 import { SecondaryButton } from "./_components/secondary-button/secondary-button";
 import { ItemCertificado } from "./_components/item-certificado/item-certificado";
+import { BaseUi } from "./_components/base-ui/base-ui";
+import { Certificados } from "./pages/certificados/certificados";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, CommonModule, PrimaryButton, SecondaryButton, ItemCertificado], // CommonModule é necessário para usar *ngIf
+  imports: [RouterOutlet, Navbar, CommonModule, PrimaryButton, SecondaryButton, ItemCertificado, BaseUi, Certificados], // CommonModule é necessário para usar *ngIf
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
